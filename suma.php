134d104004aaddc7fd4606af96bec2fe53238098
mgc182 <56601866+mgc182@users.noreply.github.com>
2019-10-25 20:12:55 -0500

    Versión final suma.php

--- suma.php
+++ suma.php
@@ -1,13 +1,6 @@
 <?php 
-//if (isset($_GET['numero1']) && isset($_GET['numero2'])) { 
-    if (isset($_GET['numero1'])) 
-    {  
-    $num1=($_GET['numero1']);
-    echo $num1;
-    }
-//$num1 = ($_GET['numero1']);
-//$num2 = ($_GET['numero2']);
- //$suma= $num1+$num2;  
-//}
-
+$num1 = $_GET['num1'];
+$num2= $_GET['num2'];
+$sum= $num1+$num2;
+echo $sum;
     ?>
